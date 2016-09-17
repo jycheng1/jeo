ea30e9d52c7a2517d74f550a29d9c849765b7086
Olivia Xu <oliviaxu@Olivias-Air.wv.cc.cmu.edu>
2016-09-17 16:58:45 -0400

    Database finished

--- jeo/models.py
+++ jeo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Event(models.Model):
+	event_name = models.CharField(max_length=100)
+	event_location_latitude = models.FloatField()
+	event_location_longitude = models.FloatField()
+	event_start_time = models.DateTimeField('Start Time')
+	event_end_time = models.DateTimeField('End Time')
+	event_category = models.CharField(max_length=20)
--- jeo/views.py
+++ jeo/views.py
@@ -1,11 +1,30 @@
 from django.shortcuts import render
 from jeo.models import *
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from pprint import pprint
+import json 
 
 import sys
 
 def index(request):
-  context = {}
+  event_list = Event.objects.order_by('event_name')
+  context = {}#event_list}
   return render(request, 'index.html', context)
+
+@csrf_exempt
+def processData(request):
+	#print("Let's see")
+	#print(request.POST)
+	eventData = request.POST['eventData']
+	decoded = json.loads(eventData)
+	pprint(decoded)
+	for elem in decoded:
+			temp = Event(event_name=elem['name'], event_location_latitude=(float)(elem['location_latitude']), 
+					 	  event_location_longitude=(float)(elem['location_longitude']), event_start_time=elem['start_time'],
+					      event_end_time=elem['end_time'], event_category=elem['category']);
+			temp.save()
+	return HttpResponse("hello!")
 
 def example(request):
   context = {}
