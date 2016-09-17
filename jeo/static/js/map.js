2a928c0e043825fa68ac158199db35641591d591
Jessica C <jycheng1@andrew.cmu.edu>
2016-09-16 21:32:54 -0400

    added location feature so map will center to your location

--- jeo/static/js/map.js
+++ jeo/static/js/map.js
@@ -1,9 +1,16 @@
 function myMap() {
   var mapCanvas = document.getElementById("map");
   var mapOptions = {
-    center: new google.maps.LatLng(40.4406,-79.9959),
+    center: new google.maps.LatLng(40.4406,-79.9959), // centered at pittsburgh
     zoom: 13,
   };
+  // if you have location services on, the map will center to your location
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+     initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+     map.setCenter(initialLocation);
+     });
+  }
 var map = new google.maps.Map(mapCanvas, mapOptions);
 }
 
