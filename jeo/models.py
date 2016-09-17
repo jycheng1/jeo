8ad56dd316882e3b285197df5a1920abb9c3477e
Jessica C <jycheng1@andrew.cmu.edu>
2016-09-17 17:38:11 -0400

    model ex

--- jeo/models.py
+++ jeo/models.py
@@ -2,9 +2,15 @@ from django.db import models
 
 # Create your models here.
 class Event(models.Model):
-	event_name = models.CharField(max_length=100)
-	event_location_latitude = models.FloatField()
-	event_location_longitude = models.FloatField()
-	event_start_time = models.DateTimeField('Start Time')
-	event_end_time = models.DateTimeField('End Time')
-	event_category = models.CharField(max_length=20)
+    event_name = models.CharField(max_length=100)
+    event_location_latitude = models.FloatField()
+    event_location_longitude = models.FloatField()
+    event_start_time = models.DateTimeField('Start Time')
+    event_end_time = models.DateTimeField('End Time')
+    event_category = models.CharField(max_length=20)
+
+    def __str__(self):
+        return (self.event_name + ' - ' + str(self.event_location_latitude) + 
+            ' - ' + str(self.event_location_longitude) + ' - ' + 
+                str(self.event_start_time) + ' - ' + 
+                str(self.event_end_time) + ' - ' + self.event_category)
